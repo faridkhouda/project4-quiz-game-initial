4eb4686be167275e07e23d60ec528ac21c4f25e6
khoudafarid <geekamazighe@gmail.com>
2023-09-08 10:19:50 +0200

    complited project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+
+createApp(App)
+.use(store)
+.use(VueAxios, axios)
+.mount('#app');
